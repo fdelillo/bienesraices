70e6b1862f92903cbe22a009a734df95ee4e9c3c
fdelillo <federicodelillo@gmail.com>
2021-10-23 07:29:53 -0300

    CRUD Terminado

--- index.php
+++ index.php
@@ -25,98 +25,13 @@
     </main>
 
     <section class="seccion contenedor">
-        <h2> Casas y Depas en Venta</h2>
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio1.webp" type="image/webp">
-                    <source srcset="build/img/anuncio1.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio1.jpg" alt="anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa de lujo en el lago</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda illum impedit. Optio dolore</p>
-                    <p class="precio">3.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--contenido-anuncio-->
-            </div><!--anuncio-->
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio2.webp" type="image/webp">
-                    <source srcset="build/img/anuncio2.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio2.jpg" alt="anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa terminada de lujo</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda illum impedit. Optio dolore</p>
-                    <p class="precio">2.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--contenido-anuncio-->
-            </div>
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio3.webp" type="image/webp">
-                    <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
-                </picture>
-                <div class="contenido-anuncio">
-                    <h3>Casa con alberca</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure assumenda illum impedit. Optio dolore</p>
-                    <p class="precio">5.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="icono habitacion">
-                            <p>4</p>
-                        </li>
-                    </ul>
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--contenido-anuncio-->
-            </div>
-        </div><!--contenedor-anuncio--> 
+        <h2>Casas y Departamentos en Venta</h2>
+        <?php 
+            $limite = 3;
+            include 'includes/templates/anuncios.php';
+        ?>
         <div class="alinear-derecha">
-            <a href="anuncios.php" class="boton-verde">Ver todas</a>
+            <a href="anuncios.php" class="boton-verde">Ver Todas</a>
         </div>
     </section>
 
